4670ee34b0272a1a9ed3aa55030ab2d798e18b50
Mahadev <Vamshi.Mahadev@innovasolutions.com>
2026-07-20 13:45:27 +0530

    fix: update snowflake provider syntax to use 'user'

--- terraform/snowflake.tf
+++ terraform/snowflake.tf
@@ -1,6 +1,6 @@
 provider "snowflake" {
   account  = var.snowflake_account
-  username = var.snowflake_user
+  user     = var.snowflake_user
   password = var.snowflake_password
   role     = "ACCOUNTADMIN"
 }
